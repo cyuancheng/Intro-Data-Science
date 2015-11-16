7bad590b8794e156478416f4a93cc0f8ce355bbb
cyuancheng <cyuancheng@gmail.com>
2015-11-16 12:12:03 -0800

    improve map figures

--- functions_nyc.py
+++ functions_nyc.py
@@ -1,4 +1,6 @@
 # statistic model
+import numpy as np
+import pandas as pd
 
 def normalize_features(array):
     """
@@ -10,6 +12,7 @@ def normalize_features(array):
     return array_normalized, mu, sigma
 
 def compute_cost(features, values, theta):
+    import numpy as np
     m = len(values)
     sum_of_square_errors = np.square(np.dot(features, theta) - values).sum()
     cost = sum_of_square_errors / (2*m)
@@ -29,15 +32,16 @@ def predictions_GD(features, values, alpha, num_iterations):
     m = len(values)
     theta = np.zeros(features.shape[1])
     theta, cost_history = gradient_descent(features, values, theta, alpha, num_iterations)
-    predictions_GD = features.dot(theta) # hypothesis function
-    predictions_GD[predictions_GD<0] = 0 # Predictions should be greater than 0
-    return theta, pd.Series(predictions_GD)
+    pred_GD = features.dot(theta) # hypothesis function
+    pred_GD[predictions_GD<0] = 0 # Predictions should be greater than 0
+    return theta, pd.Series(pred_GD)
 
 
 def getPredictions(features, intercept, params):
     """
     Calculate the predictions using the feature values and the model parameters (including intercept).
     """
+    import numpy as np
     predictions = intercept + np.dot(features.values, params)
     return predictions
 
